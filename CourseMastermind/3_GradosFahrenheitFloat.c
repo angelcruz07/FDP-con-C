d2eb78384a45ccb2ed164f4fc46921ff2acc7436
angelcruz07 <angelcruzsanchez126@gmail.com>
2024-10-15 18:04:38 -0600

    feat: add git ignore

--- CourseMastermind/3_GradosFahrenheitFloat.c
+++ CourseMastermind/3_GradosFahrenheitFloat.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 // Definición de variables
-int fahr, celsius;
+float fahr, celsius;
 int lower, upper, step;
 
 int main() {
@@ -15,9 +15,9 @@ int main() {
 
   // Bucle mientras
   while (fahr <= upper) {
-    celsius = 5 * (fahr - 32) / 9;
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
 
-    printf("%d\t%d\n", fahr, celsius);
+    printf("%3.0f\t%6.1f\n", fahr, celsius);
 
     fahr = fahr + step;
   }
