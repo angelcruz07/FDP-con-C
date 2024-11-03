36d409b4e0d8e91c50d15e6d1b0dd8d9e3051687
angelcruz07 <angelcruzsanchez126@gmail.com>
2024-11-03 13:31:32 -0600

    merge

--- CourseMastermind/Capitulo_2/Arreglos/cap2_pr01.c
+++ CourseMastermind/Capitulo_2/Arreglos/cap2_pr01.c
@@ -6,21 +6,23 @@ Uso de los arreglos o arrays
 estructura if completa y nuevas condicionales 
 (pág 16-23)
 */
-
 #include <stdio.h>
-
 
 int main(){
   int c, i, nwhite, nother;
 
   //Definition array 
-  int ndigit[10]; 
-
+  int ndigit[10];
   nwhite = nother = 0;
 
-  while(c =getchar() != EOF){
+
+  for(i = 0; i < 10; i++)
+    ndigit[i] = 0;
+
+  while((c = getchar()) != EOF){
     if(c >= '0' && c <= '9'){
       // Incrementar Array
+      ++ndigit[c - '0'];
     }
     else if (c == ' ' || c == '\n' || c == '\t'){
       nwhite++;
@@ -32,6 +34,8 @@ int main(){
 
   // Impresion de pantalla
   printf("Digitos = \n ");
+  for(i = 0; i < 10; i++)
+      printf("%d", ndigit[i]);
   printf(", espacios en blanco = %d, otros = %d\n", nwhite, nother);
 
   return 0;
---
+++ CourseMastermind/Capitulo_2/Funciones/cap2_pr2.c
@@ -0,0 +1,29 @@
+// Created by Angel on 11/3/2024.
+
+/*
+ * Una función es el equivalente
+ * a una subrutina o procedimiento
+ * en otros lenguajes.  Con la definicion
+ * de funciones es posible ignorar
+ * como se realizar para pasar
+ * a saber como se realiza (pág 26).
+ *
+ * Uso de las funciones y paso de parámetros (pág 26-29).
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+// Estos son nuestros argumentos
+int power(int base, int n) {
+  printf("d% Llego a la función power \n ");
+ return 0;
+}
+
+
+int main() {
+ int i;
+
+ //Parametró
+ power(2, 5);
+}
