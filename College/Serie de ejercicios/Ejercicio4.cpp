5a7c8cf44f8c87dbad21d9e3f94a16fd9f3e1a53
angelcruz07 <angelcruzsanchez126@gmail.com>
2024-11-20 17:48:39 -0600

    add file to exercise 4

--- College/Serie de ejercicios/Ejercicio4.cpp
+++ College/Serie de ejercicios/Ejercicio4.cpp
@@ -1,8 +1,10 @@
-// Created by Angel Cruz on 11/13/2024
-#include <iostream>
-using namespace std;                     
+/* Created by Angel Cruz on 11/13/2024
+Determinar la hipotenusa de un triángulo rectángulo conocidas las longitudes
+de sus dos catetos.
+Desarrolle el algoritmo correspondiente */
 
-int main() {
-   
-    return 0;
-}
+#include <iostream>
+
+using namespace std;
+
+int main() { return 0; }
