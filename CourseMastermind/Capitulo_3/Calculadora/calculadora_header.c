787b7b777ad55a2b665ef89de10f378f5cd971f5
angelcruz07 <angelcruzsanchez126@gmail.com>
2025-01-08 19:10:36 -0600

    feat: add libraries

--- CourseMastermind/Capitulo_3/Calculadora/calculadora_header.c
+++ CourseMastermind/Capitulo_3/Calculadora/calculadora_header.c
@@ -1,20 +1,6 @@
+#include "libreriacarac.h"
 #include <ctype.h>
 #include <stdio.h>
-
-#define MAXLINE 1000
-
-int myGetLine(char s[], int lim) {
-  int c, i;
-
-  i = 0;
-
-  while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
-    s[i++] = c;
-  if (c == '\n')
-    s[i++] = c;
-  s[i] = '\0';
-  return i;
-}
 
 int atof(char s[]) {
   double val, power;
